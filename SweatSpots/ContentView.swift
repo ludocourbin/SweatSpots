880a1e09d94b6a4aa6a548268255eafc09501cd9
ludocourbin <ludovic.courbin@gmail.com>
2023-09-06 20:17:51 +0200

    Initial Commit

---
+++ SweatSpots/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SweatSpots
+//
+//  Created by Ludovic Courbin on 06/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SweatSpots/SweatSpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweatSpotsApp.swift
+//  SweatSpots
+//
+//  Created by Ludovic Courbin on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SweatSpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
